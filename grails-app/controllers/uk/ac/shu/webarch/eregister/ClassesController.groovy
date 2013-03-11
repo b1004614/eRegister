12c3780f25705840d644235b6c760bf8123bb7c4
Rob Wallace <b1004614@my.shu.ac.uk>
2013-03-11 18:10:21 +0000

    Created REST services

--- grails-app/controllers/uk/ac/shu/webarch/eregister/ClassesController.groovy
+++ grails-app/controllers/uk/ac/shu/webarch/eregister/ClassesController.groovy
@@ -1,6 +1,26 @@
 package uk.ac.shu.webarch.eregister
 
+
+import grails.converters.*
+
 class ClassesController {
 
-    def index() { }
+    def index() {
+ 
+     def result=[:]
+
+	result.classList = []
+
+        RegClass.findAll().each {cls ->
+         result.classList.add([class_instructor:cls.class_instructor.name, 
+                               course:cls.course.courseName,
+                               className:cls.name])
+       }
+
+     withFormat {
+       html result
+       xml { render result as XML }
+       json { render result as JSON }
+    }
+  }
 }
